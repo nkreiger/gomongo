54b4a54f7cef623fc104375c42786bbe5c4782c3
Noah Kreiger <noahkreiger@Noahs-MacBook-Pro.local>
2020-12-28 12:48:52 -0500

    update

--- services/read.go
+++ services/read.go
@@ -9,30 +9,6 @@ import (
 	"time"
 )
 
-var Connection = func(w http.ResponseWriter, r *http.Request) {
-	(w).Header().Set("Content-Type", "application/json")
-
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-
-	client, err := mongo.Connect(ctx)
-	if err != nil {
-		writeStatus(&w, http.StatusBadRequest, err.Error())
-		return
-	}
-
-	defer func() {
-		if err := client.Disconnect(ctx); err != nil {
-			log.Printf("error disconnecting client: %v", err)
-		}
-
-		cancel()
-	}()
-
-	// write back okay for good connection
-	writeStatus(&w, http.StatusOK, nil)
-}
-
 func writeStatus(writer *http.ResponseWriter, statusCode int, response interface{}) {
 	(*writer).WriteHeader(statusCode)
 
